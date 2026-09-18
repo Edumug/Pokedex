5be9d6d3ce7bb1814236218dee485165158991d3
Edumug <eduardoarnhold301@gmail>
2026-09-18 16:19:26 -0300

    feat: commit final

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-const fetchPokemon = (pokemon) => {
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_img')
+const form = document.querySelector('.form')
+const input = document.querySelector('.input_search')
+const next = document.querySelector('.btn-next')
+const prev = document.querySelector('.btn-prev')
 
-const APIResponse = fetch(`https://pokeapi.co/api/v2/pokemon/115 ${pokemon}`);
-console.log()
+let searchPokemon = 1;
+
+const fetchPokemon =  async(pokemon) => {
+const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+if (APIResponse.status === 200){
+    const data = await APIResponse.json();
+    return data;
+    }
 }
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading... '
+    pokemonNumber.innerHTML = ''
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    } else{
+        pokemonName.innerHTML = 'Who is he?';
+        pokemonNumber.innerHTML = '';
+        pokemonImage.src = 'images/tung.webp'
+    }
+}
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+    input.value = '';
+});
+
+next.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+prev.addEventListener('click', () => {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+});
+renderPokemon(searchPokemon);
